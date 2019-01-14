2afe66881f7c74656fda018c0da90e36002bb821
Leon <kliwaru@gmail.com>
2019-01-14 20:17:51 +0800

    styling

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -15,19 +15,21 @@ class ListingsController < ApplicationController
         @listings = Listing.all
         min_price = Listing.all.minimum("price")
         max_price = Listing.all.maximum("price")
-        
-        if params[:min_price] || params[:max_price]
+        if params[:title] != nil
+            @listings = @listings.where('lower(title) LIKE ?', "%#{params[:title].downcase}%") #more secure? % % is wildcard in sql
+        end
+
+        if params[:min_price] != "" && params[:min_price] != nil
             min_price = params[:min_price].to_i
+            @listings = @listings.where(price: (min_price..max_price))
+        end
+
+        if params[:max_price] != "" && params[:max_price] != nil
             max_price = params[:max_price].to_i
             @listings = @listings.where(price: (min_price..max_price))
         end
         
-        if params[:title]
-            @listings = @listings.where('lower(title) LIKE ?', "%#{params[:title].downcase}%") #more secure? % % is wildcard in sql
-        end
-        
         @listings = @listings.order(:price).page (params[:page])
-        
         respond_to do |format|
             format.html    
             format.js
